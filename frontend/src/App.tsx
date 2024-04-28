d92d82cfd59b8b0e529a7cfb24d6579f68eaa57c
Bhushan Alhat <alhatbhushan@gmail.com>
2024-04-29 00:00:30 +0530

    [Frontend] feat: added text display

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,21 @@
+import words from './data/words';
+
 function App() {
-	return <h1 className='text-3xl font-bold underline text-center'>Typing</h1>;
+	return (
+		<main className='flex flex-col min-h-screen bg-slate-800'>
+			<section className='container grid items-center flex-grow mx-auto'>
+				<div className='w-1/2 mx-auto font-mono text-xl leading-relaxed tracking-wider text-gray-400'>
+					{words.map((word) => (
+						<span>
+							{word.split('').map((letter) => (
+								<span>{letter}</span>
+							))}
+						</span>
+					))}
+				</div>
+			</section>
+		</main>
+	);
 }
 
 export default App;
