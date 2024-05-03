b93aa9aa4251ec2f06af41b91a1365a007cffd47
Bhushan Alhat <alhatbhushan@gmail.com>
2024-05-03 11:51:53 +0530

    [Frontend] refactor: changed logic for timer
    use requestAnimationFrame instead of setTimeout for calculating time
    calculate wpm while updating time

--- frontend/src/hooks/useTimer.ts
+++ frontend/src/hooks/useTimer.ts
@@ -1,21 +1,46 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
-function useTimer({ currentWordIndex, currentLetterIndex }: useTimerInput) {
-	const MAX_TIME = 5;
-	const INTERVAL_MS = 1000;
+function useTimer({
+	currentWordIndex,
+	currentLetterIndex,
+	startTime,
+	calculateWPM,
+}: useTimerInput) {
+	const MAX_TIME = 15;
+	const INTERVAL_MS = 500;
 
 	const [timeLeft, setTimeLeft] = useState(MAX_TIME);
 	const [isTestRunning, setIsTestRunning] = useState(false);
 
-	useEffect(() => {
-		if (!isTestRunning) return;
-		const timer = setTimeout(() => {
-			if (timeLeft > 0) setTimeLeft((time) => time - INTERVAL_MS / 1000);
-			else setIsTestRunning(false);
-		}, INTERVAL_MS);
+	const animationFrameId = useRef<number>(0);
+	const lastUpdatedTime = useRef<number>(0);
 
-		return () => clearTimeout(timer);
-	}, [timeLeft, isTestRunning]);
+	const updateTimeLeft = useCallback(() => {
+		animationFrameId.current = requestAnimationFrame(updateTimeLeft);
+
+		const currentTime = Date.now();
+		const elapsedTime = (currentTime - (startTime.current ?? 0)) / 1000;
+		const remainingTime =
+			MAX_TIME - elapsedTime ? MAX_TIME - elapsedTime : 0;
+
+		if (currentTime - lastUpdatedTime.current >= INTERVAL_MS) {
+			if (lastUpdatedTime.current === 0) {
+				calculateWPM('gross');
+				calculateWPM('net');
+			}
+			setTimeLeft(remainingTime);
+			lastUpdatedTime.current = currentTime;
+		}
+		if (remainingTime <= 0) setIsTestRunning(false);
+	}, [startTime, calculateWPM]);
+
+	useEffect(() => {
+		if (isTestRunning) {
+			updateTimeLeft();
+		} else {
+			cancelAnimationFrame(animationFrameId.current);
+		}
+	}, [isTestRunning, updateTimeLeft]);
 
 	function resetTimer() {
 		setTimeLeft(MAX_TIME);
@@ -24,16 +49,25 @@ function useTimer({ currentWordIndex, currentLetterIndex }: useTimerInput) {
 
 	function startTestIfNeeded() {
 		if (isTestRunning) return;
-		if (currentWordIndex === 0 && currentLetterIndex === 0)
+		if (currentWordIndex === 0 && currentLetterIndex === 0) {
 			setIsTestRunning(true);
+			startTime.current = Date.now();
+		}
 	}
 
-	return { timeLeft, isTestRunning, resetTimer, startTestIfNeeded };
+	return {
+		timeLeft,
+		isTestRunning,
+		resetTimer,
+		startTestIfNeeded,
+	};
 }
 
 type useTimerInput = {
 	currentWordIndex: number;
 	currentLetterIndex: number;
+	startTime: React.MutableRefObject<number | undefined>;
+	calculateWPM: (wpmType: 'gross' | 'net') => void;
 };
 
 export default useTimer;
