e2fab3ef8a013a3bc9ade2db33057714be7bb284
Bhushan Alhat <alhatbhushan@gmail.com>
2024-05-06 13:29:01 +0530

    [Frontend] feat: show stats on test completion

--- frontend/src/components/TextDisplay.tsx
+++ frontend/src/components/TextDisplay.tsx
@@ -68,8 +68,23 @@ function TextDisplay() {
 
 	if (isTestCompleted) {
 		return (
-			<div className='flex flex-col items-center w-full gap-4'>
-				<h1 className='text-3xl text-white'>Test Completed</h1>
+			<div className='flex flex-col items-center w-full gap-4 text-white'>
+				<h1 className='text-3xl text-gray-500'>Test Completed</h1>
+				<div className='grid items-center grid-cols-3 text-center gap-y-4 gap-x-8'>
+					<div className='col-span-3'>Your stats</div>
+					<div className='row-span-2'>
+						<div>Gross WPM</div>
+						<div className='text-7xl'>{grossWPM.toFixed(0)}</div>
+					</div>
+					<div className='row-span-2'>
+						<div>Net WPM</div>
+						<div className='text-7xl'>{netWPM.toFixed(0)}</div>
+					</div>
+					<div className='row-span-2'>
+						<div>Accuracy</div>
+						<div className='text-7xl'>{accuracy.toFixed(0)}%</div>
+					</div>
+				</div>
 				<ResetButton reset={reset} label='Start Again' autoFocus />
 			</div>
 		);
