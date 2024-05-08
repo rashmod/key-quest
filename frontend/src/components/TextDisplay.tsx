631b47732236a5180e62afa6446deb09c85de8c3
Bhushan Alhat <alhatbhushan@gmail.com>
2024-05-08 20:44:53 +0530

    [Frontend] refactor: created separate component for results page

--- frontend/src/components/TextDisplay.tsx
+++ frontend/src/components/TextDisplay.tsx
@@ -1,35 +1,15 @@
 import HiddenInput from './HiddenInput';
-import ResetButton from './ResetButton';
 import WordsContainer from './WordsContainer';
 import Stats from './Stats';
 
 import useTestContext from '../context/TestContext/useTestContext';
+import ResultPage from '../pages/ResultPage';
 
 function TextDisplay() {
-	const { grossWPM, netWPM, accuracy, isTestCompleted } = useTestContext();
+	const { isTestCompleted } = useTestContext();
 
 	if (isTestCompleted) {
-		return (
-			<div className='flex flex-col items-center w-full gap-4 text-white'>
-				<h1 className='text-3xl text-gray-500'>Test Completed</h1>
-				<div className='grid items-center grid-cols-3 text-center gap-y-4 gap-x-8'>
-					<div className='col-span-3'>Your stats</div>
-					<div className='row-span-2'>
-						<div>Gross WPM</div>
-						<div className='text-7xl'>{grossWPM.toFixed(0)}</div>
-					</div>
-					<div className='row-span-2'>
-						<div>Net WPM</div>
-						<div className='text-7xl'>{netWPM.toFixed(0)}</div>
-					</div>
-					<div className='row-span-2'>
-						<div>Accuracy</div>
-						<div className='text-7xl'>{accuracy.toFixed(0)}%</div>
-					</div>
-				</div>
-				<ResetButton label='Start Again' autoFocus />
-			</div>
-		);
+		return <ResultPage />;
 	}
 
 	return (
