85952fc54f55a35201759b7c3f5b6ae402a0c83d
Bhushan Alhat <alhatbhushan@gmail.com>
2024-05-08 22:51:02 +0530

    [Backend] chore: process and store word frequency file

---
+++ backend/src/utilities/dataProcessor.ts
@@ -0,0 +1,20 @@
+import { readFileSync, writeFileSync } from 'fs';
+import path from 'path';
+
+function processData(data: string): string {
+	return data
+		.split('\n')
+		.map((x) => x.split('\t')[0])
+		.join('\n');
+}
+
+function readAndWriteData() {
+	const data = readFileSync(
+		path.join(__dirname, 'inputs', 'word_frequency.txt'),
+		'utf8'
+	);
+	const cleanedData = processData(data);
+	writeFileSync(path.join(__dirname, 'inputs', 'word.txt'), cleanedData);
+}
+
+export default readAndWriteData;
