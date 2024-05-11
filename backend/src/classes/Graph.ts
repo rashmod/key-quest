e82aed4c229b9d842b43e91079c5ed8e8b9c2ecc
Bhushan Alhat <alhatbhushan@gmail.com>
2024-05-11 18:15:36 +0530

    [Backend] feat: connect graph using letters as nodes

--- backend/src/classes/Graph.ts
+++ backend/src/classes/Graph.ts
@@ -1,3 +1,4 @@
+import Nodes from '../constants/Nodes';
 import FileHelper from './FileHelper';
 
 class Graph {
@@ -7,6 +8,7 @@ class Graph {
 
 	constructor() {
 		this.initializeGraph();
+		this.connectGraph();
 	}
 
 	private initializeGraph() {
@@ -18,6 +20,35 @@ class Graph {
 		this.graphData = Array.from({ length: this.size }, () =>
 			Array(this.size).fill(0)
 		);
+	}
+
+	private connectGraph() {
+		for (let i = 0; i < this.data.length; i++) {
+			const word = this.data[i];
+
+			for (let j = 0; j < word.length; j++) {
+				const letter = word[j] as keyof typeof Nodes;
+				const nextLetter = word[j + 1] as keyof typeof Nodes;
+
+				if (j === 0) {
+					this.incrementNode('start', letter);
+					this.incrementNode('start', 'total');
+				} else if (j === word.length - 1) {
+					this.incrementNode(letter, 'end');
+					this.incrementNode(letter, 'total');
+				} else {
+					this.incrementNode(letter, nextLetter);
+					this.incrementNode(letter, 'total');
+				}
+			}
+		}
+	}
+
+	private incrementNode(
+		node: keyof typeof Nodes,
+		nextNode: keyof typeof Nodes
+	) {
+		this.graphData[Nodes[node]][Nodes[nextNode]] += 1;
 	}
 }
 
